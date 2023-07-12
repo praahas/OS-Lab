c0b544ac31fdae716c11f3d899a08c4bdae36cd6
Flashflow <praahas@gmail.com>
2023-07-12 11:14:52 +0530

    Update WORST_FIT.c

--- 4 - Memory Allocation Techniques/WORST_FIT.c
+++ 4 - Memory Allocation Techniques/WORST_FIT.c
@@ -3,9 +3,12 @@
 #define max 25
 void main()
 {
-    int frag[max], b[max], f[max], i, j, nb, nf, temp;
+    int
+        frag[max],
+        b[max], f[max], i, j, nb, nf, temp, highest = 0;
     static int bf[max], ff[max];
-    printf("\n\tMemory Management Scheme - First Fit");
+    clrscr();
+    printf("\n\tMemory Management Scheme - Worst Fit");
     printf("\nEnter the number of blocks:");
     scanf("%d", &nb);
     printf("Enter the number of files:");
@@ -26,21 +29,22 @@ void main()
     {
         for (j = 1; j <= nb; j++)
         {
-            if (bf[j] != 1)
+            if (bf[j] != 1) // if bf[j] is not allocated
             {
                 temp = b[j] - f[i];
                 if (temp >= 0)
-                {
-                    ff[i] = j;
-                    break;
-                }
+                    if (highest < temp)
+                    {
+                    }
             }
+            frag[i] = highest;
+            bf[ff[i]] = 1;
+            highest = 0;
         }
-        frag[i] = temp;
-        bf[ff[i]] = 1;
+        ff[i] = j;
+        highest = temp;
     }
-    printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
+    printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement");
     for (i = 1; i <= nf; i++)
         printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, f[i], ff[i], b[ff[i]], frag[i]);
-    getch();
 }
